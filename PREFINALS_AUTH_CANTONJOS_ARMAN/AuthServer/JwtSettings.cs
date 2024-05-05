04960e6bddb8db2f5e04d126fecc8d020f9d4606
CantonjosArman-BSIT32E1 <160833955+CantonjosArman-BSIT32E1@users.noreply.github.com>
2024-05-05 20:05:11 +0800

    Fixing Bugs

--- PREFINALS_AUTH_CANTONJOS_ARMAN/AuthServer/JwtSettings.cs
+++ PREFINALS_AUTH_CANTONJOS_ARMAN/AuthServer/JwtSettings.cs
@@ -1,9 +1,7 @@
-﻿namespace AuthServer
-{
+﻿
     public class JwtSettings
     {
         public string? Issuer { get; set; }
         public string? Audience { get; set; }
         public int ExpiryInMinutes { get; set; }
     }
-}
