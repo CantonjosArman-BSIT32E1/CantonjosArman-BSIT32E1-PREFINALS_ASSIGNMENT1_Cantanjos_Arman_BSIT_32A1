e95cc2c250cbee22b1379768429d9ffe41f202f0
CantonjosArman-BSIT32E1 <160833955+CantonjosArman-BSIT32E1@users.noreply.github.com>
2024-05-05 20:14:46 +0800

    working na

--- PREFINALS_AUTH_CANTONJOS_ARMAN/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
+++ PREFINALS_AUTH_CANTONJOS_ARMAN/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c832e9dcfd1489aa1c88999eb96a9ef9fae116c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d191e8a3c225746038d101a874fd4a05463dd6c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
