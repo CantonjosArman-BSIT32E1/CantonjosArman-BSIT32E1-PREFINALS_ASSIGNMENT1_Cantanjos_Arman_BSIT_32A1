3b542db16e2fafb19d8817fc824ee3e5d963b2d3
CantonjosArman-BSIT32E1 <160833955+CantonjosArman-BSIT32E1@users.noreply.github.com>
2024-05-05 20:06:25 +0800

    editing code

--- PREFINALS_AUTH_CANTONJOS_ARMAN/AuthServer/Program.cs
+++ PREFINALS_AUTH_CANTONJOS_ARMAN/AuthServer/Program.cs
@@ -1,21 +1,20 @@
-using AuthServer;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Add JWT settings configuration
+
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
 
-//required for controllers
+
 builder.Services.AddControllers();
 
-// Configure authentication
+
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
@@ -33,7 +32,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
