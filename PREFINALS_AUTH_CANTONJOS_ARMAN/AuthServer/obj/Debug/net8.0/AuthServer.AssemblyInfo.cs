5c832e9dcfd1489aa1c88999eb96a9ef9fae116c
CantonjosArman-BSIT32E1 <160833955+CantonjosArman-BSIT32E1@users.noreply.github.com>
2024-05-05 19:58:33 +0800

    Add folder Controllers and add class and add code

--- PREFINALS_AUTH_CANTONJOS_ARMAN/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
+++ PREFINALS_AUTH_CANTONJOS_ARMAN/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33b17065800182da23aa5a15b56342b150622bc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cddae22ee51034808b29f1f0fce66eb1084eb4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
