03f0a55688ac9816a2e64d9f0790882570d1f6ec
CantonjosArman-BSIT32E1 <160833955+CantonjosArman-BSIT32E1@users.noreply.github.com>
2024-05-05 20:20:08 +0800

    startup.cs put code

--- PREFINALS_AUTH_CANTONJOS_ARMAN/ProtectedApi/obj/Debug/net8.0/ProtectedApi.AssemblyInfo.cs
+++ PREFINALS_AUTH_CANTONJOS_ARMAN/ProtectedApi/obj/Debug/net8.0/ProtectedApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProtectedApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0d18f4871d5b4fc1aff3620d6e9572e5eca787b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a99cd06ddd16b6cebca63ba752267d62cd70ede7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProtectedApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProtectedApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
